67a2c33e51769aa030f1ee11f692cf36f1cd28fd
ortizjulian <julian.ortixs@gmail.com>
2024-08-14 17:28:04 -0500

    Initial project

---
+++ src/main/java/com/personas/api_personas/ApiPersonasApplication.java
@@ -0,0 +1,13 @@
+package com.personas.api_personas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPersonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPersonasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personas/api_personas/ApiPersonasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personas.api_personas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPersonasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
